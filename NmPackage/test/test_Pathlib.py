a2bc7d4d1e6c78e025a0a6a142eb0cc68ca763d3
JDVlieger <jeroen.devlieger@nikon.com>
2018-12-29 14:03:30 +0100

    test pathlib.Path functionality

---
+++ NmPackage/test/test_Pathlib.py
@@ -0,0 +1,32 @@
+"""
+"""
+
+def test_Pathlib():
+    """
+    Test pathlib.Path parent properties
+    """
+    from pathlib import Path
+    
+    p = Path(r"$(NmPackageDir)\<packageId>\<versionId>\NmPackage.props")
+    assert r"$(NmPackageDir)\<packageId>\<versionId>\NmPackage.props" == str(p)
+    assert "NmPackage.props" == p.name
+
+    p = p.parent
+    assert r"$(NmPackageDir)\<packageId>\<versionId>" == str(p)
+    assert "<versionId>" == p.name
+
+    p = p.parent
+    assert r"$(NmPackageDir)\<packageId>" == str(p)
+    assert "<packageId>" == p.name
+
+    p = p.parent
+    assert r"$(NmPackageDir)" == str(p)
+    assert "$(NmPackageDir)" == p.name
+
+
+
+
+# run this module though pytest if executed as a script
+if __name__ == '__main__':
+    import pytest
+    pytest.main([__file__])
