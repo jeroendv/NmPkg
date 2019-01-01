7ac12473c9db9d92ca96a3e551bcec9526bd26d9
JDVlieger <jeroen.devlieger@nikon.com>
2019-01-01 14:45:15 +0100

    cleanup test_Integrate.py

--- NmPackage/test/test_Integrate.py
+++ NmPackage/test/test_Integrate.py
@@ -9,109 +9,76 @@ from NmPackage import *
 from NmPackage.save import *
 from NmPackage.test import *
 
-class chdir():
-    def __init__(self, newPath):
-        self.newPath = newPath
-    
-    def __enter__(self):
-
-        if (not Path(self.newPath).is_dir()):
-            raise Exception("Can not change to a directory that does not exist.")
-
-        self.oldPath = os.getcwd()
-        os.chdir(self.newPath)
-        return Path()
-
-    def __exit__(self, type, value, tb):
-        os.chdir(self.oldPath)
-
-
 class Test_Integrate:
 
-
-    testFilesPre = Path(__file__).parent / Path("TestFiles/VsProjectIntegration.pre")
-    testFilesVerified = testFilesPre.with_name("VsProjectIntegration.verified")
-
+    testFilesPre = (Path(__file__).parent / Path("TestFiles/VsProjectIntegration.pre")).absolute()
+    testFilesVerified =  (Path(__file__).parent / Path("TestFiles/VsProjectIntegration.verified")).absolute()
 
     def setUp(self, tmpdir):
+        """Copy `testFilesPre` to the test dir `tmpdir`"""
         print(str(tmpdir))
         from distutils.dir_util import copy_tree
         copy_tree(self.testFilesPre, str(tmpdir))
 
     def test_creationOfFiles(self, tmpdir):
-        """Verify  that that appropriate files are created
-        """
+        """Verify  that that appropriate files are created"""
+        # Given a clean VsProject
         self.setUp(tmpdir)
-        with chdir(Path(tmpdir) / Path("Vs2017Project")) as p:
-            vsProjectPath = p.absolute().joinpath("Vs2017Project.vcxproj")
+        p = Path(tmpdir) / Path("Vs2017Project")
+        os.chdir(p)
 
-            # Test pre-conditions
-            assert not Path("Vs2017Project.NmPackageDeps.props").is_file(), \
-                "before integration there should not be a 'Vs2017Project.NmPackageDeps.props'"
+        assert not Path("Vs2017Project.NmPackageDeps.props").is_file(), \
+            "before integration there should not be a 'Vs2017Project.NmPackageDeps.props'"
 
-            # test
-            integrate_vsproject(VsProject(vsProjectPath))
+        # WHEN Integrating into said project
+        integrate_vsproject(VsProject(Path("Vs2017Project.vcxproj")))
 
-            # test post-conditions
-            # verify that requried files were created
-            assert Path("Vs2017Project.NmPackageDeps.props").is_file(), \
-                "after integration a 'Vs2017Project.NmPackageDeps.props' file should be present"
+        # THEN 
+        assert Path("Vs2017Project.NmPackageDeps.props").is_file(), \
+            "after integration a 'Vs2017Project.NmPackageDeps.props' file should be present"
         
     def test_TargetsIntegration(self, tmpdir):
-        """Verify that the 'Vs2017Project.NmPackageDeps.props' is properly imported in project file
-        """
+        """Verify that the 'Vs2017Project.NmPackageDeps.props' is properly imported in project file"""
+        # Given a clean VsProject
         self.setUp(tmpdir)
-        with chdir(Path(tmpdir) / Path("Vs2017Project")) as p:
-            # test
-            integrate_vsproject(VsProject(p.absolute().joinpath("Vs2017Project.vcxproj")))
-
-
-            # test post-conditions
-            # search for the following node under projet file xml root 
-            #       <Import Project="Vs2017Project.NmPackageDeps.props" />
-            projTree = ET.parse(Path("Vs2017Project.vcxproj"))
-            root = projTree.getroot()
-            ns = {'default': 'http://schemas.microsoft.com/developer/msbuild/2003'}
-
-            for c in root.findall("default:Import", ns):
-                print(c.tag, c.attrib)
-                if ('Project' in c.attrib 
-                    and c.get('Project') == 'Vs2017Project.NmPackageDeps.props'):
-                    # node found :-)
-                    return
+        p= Path(tmpdir) / Path("Vs2017Project")
+        os.chdir(p)
         
-            assert False, "no import node found that imports Vs2017Project.NmPackageDeps.props"
-
-    def test_conanFileIntegration(self, tmpdir):
-        """Verify that the 'Vs2017Project.NmPackageDeps.props' is properly imported in project file
-        """
-        self.setUp(tmpdir)
-        with chdir(Path(tmpdir) / Path("Vs2017Project")) as p:
-            # test pre-conditions
-            pass
-
-            # test
-            integrate_vsproject(VsProject(p.absolute().joinpath("Vs2017Project.vcxproj")))
+        # WHEN Integrating into the project folder
+        integrate_vsproject(VsProject(Path("Vs2017Project.vcxproj")))
 
 
-            # test post-conditions
-            # search for the following node under the project file xml root 
-            #       <ItemGroup>
-            #         <Text Include="Vs2017Project.NmPackageDeps.props" />
-            #       </ItemGroup>
-            # to the project file
-            projTree = ET.parse(Path("Vs2017Project.vcxproj"))
-            root = projTree.getroot()
-            ns = {'default': 'http://schemas.microsoft.com/developer/msbuild/2003'}
+        # THEN the verify that the *.vcxproj file has an xml element Project/Import as follows
+        #       <Import Project="Vs2017Project.NmPackageDeps.props" />
+        projTree = ET.parse(Path("Vs2017Project.vcxproj"))
+        root = projTree.getroot()
+        ns = {'default': 'http://schemas.microsoft.com/developer/msbuild/2003'}
 
-            for c in root.findall("default:ItemGroup/default:Text", ns):
-                print(c.tag, c.attrib)
-                if ('Include' in c.attrib 
-                    and c.attrib['Include'] == 'Vs2017Project.NmPackageDeps.props'):
-                    # node found :-)              
-                    return 
-        
-            assert False, "Vs2017Project.NmPackageDeps.props include is missing from *.vcxproj file"
+        for c in root.findall("default:Import", ns):
+            print(c.tag, c.attrib)
+            if ('Project' in c.attrib 
+                and c.get('Project') == 'Vs2017Project.NmPackageDeps.props'):
+                # node found :-)
+                return
+    
+        assert False, "no import node found that imports Vs2017Project.NmPackageDeps.props"
+
+        # THEN the verify that the *.vcxproj file has an xml element Project/ItemGroup/Text as follows
+        #       <ItemGroup>
+        #         <Text Include="Vs2017Project.NmPackageDeps.props" />
+        #       </ItemGroup>
+        projTree = ET.parse(Path("Vs2017Project.vcxproj"))
+        root = projTree.getroot()
+        ns = {'default': 'http://schemas.microsoft.com/developer/msbuild/2003'}
+
+        for c in root.findall("default:ItemGroup/default:Text", ns):
+            print(c.tag, c.attrib)
+            if ('Include' in c.attrib 
+                and c.attrib['Include'] == 'Vs2017Project.NmPackageDeps.props'):
+                # node found :-)              
+                return 
+    
+        assert False, "Vs2017Project.NmPackageDeps.props include is missing from *.vcxproj file"
 
     def test_regressionTest(self, tmpdir):
         """
@@ -120,83 +87,106 @@ class Test_Integrate:
         equals the baseline result:
                 NmPackage\test\TestFiles\VsProjectIntegration.verified\Vs2017Project\
         """
+        # Given a clean VsProject
         self.setUp(tmpdir)
-        verifiedDir = self.testFilesVerified.absolute()
-        with chdir(Path(tmpdir) / Path("Vs2017Project")) as p:
-            # test
-            integrate_vsproject(VsProject(p.absolute().joinpath("Vs2017Project.vcxproj")))
+        p= Path(tmpdir) / Path("Vs2017Project")
+        os.chdir(p)
 
-            compare_dirs(verifiedDir, Path(tmpdir))
+        # WHEN Integrating into the project dir
+        integrate_vsproject(VsProject(Path("Vs2017Project.vcxproj")))
+
+        # THEN the resulting dir should match the baseline
+        compare_dirs(self.testFilesVerified, Path(tmpdir))
 
 
     def test_integrate_given_dir(self, tmpdir):
         """
         check that a directory containing a single *.vcxproj file can be integrated
         """
+        # GIVEN a clean dir with a single clean project
         self.setUp(tmpdir)
         verifiedDir = self.testFilesVerified.absolute()
-        with chdir(Path(tmpdir) / Path("Vs2017Project")) as p:
-            # test
-            Integrate(p.absolute())
-            
-            compare_dirs(verifiedDir, Path(tmpdir))
+        os.chdir(Path(tmpdir) / Path("Vs2017Project"))
+
+        # WHEN Intregration into the dir
+        Integrate(".")
+        
+        # THEN the result dir should be identical to the baseline
+        compare_dirs(verifiedDir, Path(tmpdir))
     
+    def test_cli_call(self, tmpdir):
+        """
+        check that a directory containing a single *.vcxproj file can be integrated though the command line
+        """
+        # given a clean project
+        self.setUp(tmpdir)
+        verifiedDir = self.testFilesVerified.absolute()
+        os.chdir(Path(tmpdir) / Path("Vs2017Project")) 
+
+        # WHEN calling the cli main function with the debug option
+        import NmPackage.cli.integrate
+        sys.argv = ['arg0', '-d']
+        NmPackage.cli.integrate.main()
+
+        # THEN the result should match the baseline
+        compare_dirs(verifiedDir, Path(tmpdir))
+    
+class Test_Integrate_error_flows():
+
     def test_fail_on_nonexisting_file(self):
+
+        # WHEN integrating into a non-existing file
         with pytest.raises(Exception) as e:
             Integrate("non-existing_file.txt")
             
+        # THEN this should fail with a proper error message
         assert "expected *.vcxproj file or dir containing a single *.vcxproj file" in str(e.value)
     
     def test_fail_for_non_vcxprojfile(self, tmpdir):
-        with chdir(str(tmpdir)):
-            with Path("non-vcxprojFile.txt").open("wt") as f:
-                f.write("")
-            with pytest.raises(Exception) as e:
-                Integrate("non-vcxprojFile.txt")
+        os.chdir(str(tmpdir))
+        # GIVEN a non *.vcxproj file
+        non_vcxproj_file = Path("non-vcxprojFile.txt")
+        with non_vcxproj_file.open("wt") as f:
+            f.write("")
 
-            assert "file is not a *.vcxproj file" in str(e.value)
+        # WHEN Integration into that file
+        with pytest.raises(Exception) as e:
+            Integrate("non-vcxprojFile.txt")
+
+        # THEN this should fail with a proper error message
+        assert "file is not a *.vcxproj file" in str(e.value)
 
 
     def test_fail_on_dir_without_project(self,tmpdir):
-
-        with chdir(str(tmpdir)):
-            with pytest.raises(Exception) as e:
-                Integrate(".")
-            
-            assert "no project files found in the given directory:" in str(e.value)
+        os.chdir(str(tmpdir))
+        # GIVEN a dir without a *.vxcproj file
+        assert not list(Path(".").glob("*.vcxproj"))
+        
+        # WHEN integrating into the dir 
+        with pytest.raises(Exception) as e:
+            Integrate(".")
+        
+        # THEN this should fail with a proper error message
+        assert "no project files found in the given directory:" in str(e.value)
 
     def test_fail_on_dir_with_multiple_projects(self,tmpdir):
+        os.chdir(tmpdir)
+        # GIVEN a dir with multiple *.vcxprojects
+        with Path("proj1.vcxproj").open("wt") as f:
+            f.write("")
 
-        with chdir(str(tmpdir)):
-            with Path("proj1.vcxproj").open("wt") as f:
-                f.write("")
-
-            with Path("proj2.vcxproj").open("wt") as f:
-                f.write("")
-
-            with pytest.raises(Exception) as e:
-                Integrate(".")
-            
-            assert "multiple project files found." in str(e.value)
+        with Path("proj2.vcxproj").open("wt") as f:
+            f.write("")
         
+        assert len(list(Path(".").glob("*.vcxproj"))) > 1
 
+        # WHEN integrating into the dir
+        with pytest.raises(Exception) as e:
+            Integrate(".")
         
+        # THEN this should fail with a proper error message
+        assert "multiple project files found." in str(e.value)
 
-
-    def test_cli_call(self, tmpdir):
-        """
-        check that a directory containing a single *.vcxproj file can be integrated
-        """
-        self.setUp(tmpdir)
-        verifiedDir = self.testFilesVerified.absolute()
-        with chdir(Path(tmpdir) / Path("Vs2017Project")) as p:
-            # WHEN calling the cli main function with the debug option
-            import NmPackage.cli.integrate
-            sys.argv = ['arg0', '-d']
-            NmPackage.cli.integrate.main()
-
-            # THEN the result should match the baseline
-            compare_dirs(verifiedDir, Path(tmpdir))
 
 
    
