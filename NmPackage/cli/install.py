22241614da3970574ea36fa1576d8bba0a8bb977
JDVlieger <jeroen.devlieger@nikon.com>
2019-01-28 09:53:39 +0100

    improve NmPkg-install cli command
    * allow passing multiple packageId's on the cli
    * add debug logging
    * allow passing a dir tree to recursively find all *.NmPackageDeps.props file

--- NmPackage/cli/install.py
+++ NmPackage/cli/install.py
@@ -20,12 +20,15 @@ def parse_cli_args():
                         help="enable debug output",
                         action="store_true")
 
-    parser.add_argument("qualifiedPackageId",
+    parser.add_argument("qualifiedPackageIds",
                         help="qualifiedId of the package to be added",
-                        nargs="?")
+                        nargs="*")
 
     parser.add_argument("--vcxproj",
                         help="path to the folder containing a single *.vcxproj file or a specific *.vcxproj file")
+
+    parser.add_argument("--dirtree",
+                        help="path to directory tree for installation of all *.NmPackageDeps.props")
 
     parser.add_argument("-N", "--dry-run",
                         help="Do not perform any actions, only simulate them.",
@@ -51,8 +54,8 @@ def main():
 
     # collect all packages to be installed
     packages = set()
-    if args.qualifiedPackageId is not None:
-        packages.add(NmPackageId.from_qualifiedId(args.qualifiedPackageId))
+    for id in args.qualifiedPackageIds:
+        packages.add(NmPackageId.from_qualifiedId(id))
 
     if args.vcxproj is not None:
         try:
@@ -64,11 +67,59 @@ def main():
         vsProject = VsProjectFiler().deserialize(vcxproj_file)
         packages.update(vsProject.dependencies)
 
+    if args.dirtree is not None:
+        dirtree = Path(args.dirtree)
+
+        if not dirtree.is_dir():
+            raise Exception("unknown directory tree: " + args.dirtree)
+
+        packages.update(collect_all_packages(dirtree))
+
     with DebugLogScopedPush("found packages:"):
         for p in packages:
             DebugLog.print(p.qualifiedId)
 
+    # if dry-run then don't actually proceed to install all packages
+    if args.dry_run:
+        return
+
     # install all of the packages
     mgr = NmPackageManager.get_system_manager()
     for p in packages:
+        DebugLog.print("installing NmPackage: " + p.qualifiedId)
         mgr.install(p)
+
+
+
+   
+
+def collect_all_packages(tree: Path) -> set:
+    """ 
+    recursively find all *.NmPackageDeps.props and collect all packages
+    """
+    packages = set()
+
+    assert Path(tree).is_dir()
+
+    # traverse the tree ignoring vcs folders
+    ignoreFolders = ['.svn', '.git']
+    for root, dirs, files in os.walk(tree):
+        for f in ignoreFolders:
+            if (f in dirs):
+                dirs.remove(f) 
+
+        # get list of all NmPackage listengs in 'root'
+        nm_package_deps_files = [Path(root)/Path(f) for f in files if f.endswith(".NmPackageDeps.props")]
+
+        # collect all the NmPackageId's
+        for file in nm_package_deps_files:
+            DebugLog.print("found: " + str(file))
+            with file.open("tr") as f:
+                packages.update(NmPackageDepsFileFormat.deserialize(f.read()))\
+    
+    return packages
+        
+
+
+   
+
